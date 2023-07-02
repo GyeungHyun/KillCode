314152b6a99403c9e99618bb6a32582283be319a
anzuan04 <137792782+anzuan04@users.noreply.github.com>
2023-07-03 04:39:48 +0900

    캐릭터 움직임

---
+++ KillCode/Assets/script/Main_Character/Main_Move.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Move : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput);
+        moveDirection.Normalize();
+
+        rb.velocity = moveDirection * moveSpeed;
+
+        Lookmouse();
+    }
+
+    public void Lookmouse()
+    {
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Plane GroupPlane = new Plane(Vector3.up, Vector3.zero);
+
+        float rayLength;
+
+        if (GroupPlane.Raycast(cameraRay, out rayLength))
+
+        {
+
+            Vector3 pointTolook = cameraRay.GetPoint(rayLength);
+
+            transform.LookAt(new Vector3(pointTolook.x, transform.position.y, pointTolook.z));
+
+        }
+    }
+}
